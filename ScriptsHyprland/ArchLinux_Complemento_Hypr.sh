72ba5b3e33a4b7f2cc56a988fffc3fcc826890b2
Alexandre Garcia dos Santos <135478609+codedbyxande@users.noreply.github.com>
2025-05-21 12:10:16 -0300

    Add files via upload

---
+++ ScriptsHyprland/ArchLinux_Complemento_Hypr.sh
@@ -0,0 +1,11 @@
+
+echo -e "${CYAN}\n===== INSTALANDO PARU (AUR HELPER) =====${NC}"
+temp_dir=$(mktemp -d)
+echo "Usando diretório temporário: $temp_dir"
+pacman -S --needed base-devel git --noconfirm
+git clone https://aur.archlinux.org/paru.git "$temp_dir/paru"
+(cd "$temp_dir/paru" && makepkg -si --noconfirm)
+rm -rf "$temp_dir"
+
+echo -e "${CYAN}\n===== INSTALANDO VS CODE =====${NC}"
+paru -S visual-studio-code-bin --noconfirm
