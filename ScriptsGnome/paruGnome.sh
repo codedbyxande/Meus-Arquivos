bcf2874691fa73c5af5c8f58e745c617e3a3af0c
Alexandre Garcia dos Santos <135478609+codedbyxande@users.noreply.github.com>
2025-05-23 09:49:58 -0300

    Add files via upload

---
+++ ScriptsGnome/paruGnome.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# Define colors for output
+CYAN='\033[0;36m'
+NC='\033[0m' # No Color
+
+echo -e "${CYAN}\n===== INSTALANDO PARU (AUR HELPER) =====${NC}"
+mkdir -p tmp # -p ensures no error if tmp exists
+cd tmp
+git clone https://aur.archlinux.org/paru.git
+cd paru
+makepkg -si # This will prompt for sudo password and confirmation
+cd ../.. # Go back to the original directory
+rm -rf tmp # Remove the temporary directory
+
+echo -e "${CYAN}\n===== INSTALANDO VS CODE =====${NC}"
+paru -S visual-studio-code-bin --noconfirm
+
+echo -e "${CYAN}\n===== INSTALANDO ZEN  =====${NC}"
+paru -S --noconfirm zen-browser-bin
+
+echo -e "${CYAN}\n===== INSTALANDO EXTENSÃO POP SHELL =====${NC}"
+paru -S --noconfirm gnome-shell-extension-pop-shell-git
+
+echo -e "${CYAN}\n===== INSTALAÇÃO CONCLUÍDA! =====${NC}"
